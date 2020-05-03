98462ce6428511b3ae94419e9c238436ddb08947
dzony6969 <dzony6969@users.noreply.github.com>
2020-05-03 11:10:51 +0200

    before database was added

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,34 +1,71 @@
-const Product = require("../models/product");
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render("admin/add-product", {
-    pageTitle: "Add Product",
-    path: "/admin/add-product",
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true,
+  res.render('admin/edit-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    editing: false
   });
 };
 
 exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const image = req.body.image;
-  const product = new Product(title, description, price, image);
+  const product = new Product(null, title, imageUrl, description, price);
   product.save();
-  res.redirect("/");
+  res.redirect('/');
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect('/');
+  }
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    if (!product) {
+      return res.redirect('/');
+    }
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product: product
+    });
+  });
+};
+
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDesc = req.body.description;
+  const updatedProduct = new Product(
+    prodId,
+    updatedTitle,
+    updatedImageUrl,
+    updatedDesc,
+    updatedPrice
+  );
+  updatedProduct.save();
+  res.redirect('/admin/products');
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render("admin/products", {
+  Product.fetchAll(products => {
+    res.render('admin/products', {
       prods: products,
-      pageTitle: "admin products",
-      path: "/admin/products",
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true,
+      pageTitle: 'Admin Products',
+      path: '/admin/products'
     });
   });
+};
+
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.deleteById(prodId);
+  res.redirect('/admin/products');
 };
--- routes/shop.js
+++ routes/shop.js
@@ -1,16 +1,25 @@
-const express = require("express");
-const productsController = require("../controllers/shop");
+const path = require('path');
+
+const express = require('express');
+
+const shopController = require('../controllers/shop');
+
 const router = express.Router();
 
-router.get("/", productsController.getIndex);
+router.get('/', shopController.getIndex);
 
-router.get("/products", productsController.getProducts);
+router.get('/products', shopController.getProducts);
 
-router.get("/products/:productId", productsController.getProduct);
+router.get('/products/:productId', shopController.getProduct);
 
-router.get("/cart", productsController.getCart);
+router.get('/cart', shopController.getCart);
 
-router.get("/checkout", productsController.getCheckout);
-router.get("/orders", productsController.getOrders);
+router.post('/cart', shopController.postCart);
+
+router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
